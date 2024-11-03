25ae348d1dedc6507b9dd5c38f087d8076526414
Tomáš Barták <tomasbartak12@email.cz>
2024-11-03 15:31:55 +0100

    feat: starting list formatting + category prototype

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Category;
 use App\Http\Controllers\ContestController;
 
 class AdminController extends Controller
--- resources/views/admin/starting-list.blade.php
+++ resources/views/admin/starting-list.blade.php
@@ -1,12 +1,24 @@
 <section>
     <h3 class="font-semibold">Starting List:</h3>
 
-    <!-- Input for year -->
-    <div>
-        <x-input-label for="year" :value="__('Year') . '*'" />
-        <x-text-input id="year-starting-list" name="year" type="number" class="mt-1 block w-half" value="{{ old('year', date('Y')) }}" required
-            min="2000" max="2100" />
-        <x-input-error class="mt-2" :messages="$errors->get('year')" />
+    <div class="flex gap-4">
+        <!-- Input for year -->
+        <div>
+            <x-input-label for="year" :value="__('Year')" required="true" />
+            <x-text-input id="year-starting-list" name="year" type="number" class="mt-1 block w-half" value="{{ old('year', date('Y')) }}" required
+                min="2000" max="2100" />
+            <x-input-error class="mt-2" :messages="$errors->get('year')" />
+        </div>
+
+        <!-- Category Select -->
+        <div>
+            <x-input-label for="category" :value="__('Category')" required="true" />
+            <select id="category-select" name="category" class="block mt-1 w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm" required>
+                <option value="" disabled selected>Select a category</option>
+                <!-- přidat konkrétní kategorie... -->
+            </select>
+            <x-input-error class="mt-2" :messages="$errors->get('category')" />
+        </div>
     </div>
 
     <!-- Button to generate starting list and the starting list -->
@@ -21,7 +33,9 @@
     document.getElementById('generate-starting-list').addEventListener('click', function () {
 
         const yearInput = document.getElementById('year-starting-list');
+        // const categorySelect = document.getElementById('category-select');
         const year = yearInput.value;
+        // const category = categorySelect.value;
 
         fetch(`/admin/generate-starting-list/${year}`, {
             method: 'POST',
@@ -37,7 +51,14 @@
 
                 if (data.length > 0) {
                     data.forEach(item => {
-                        const robotEntry = `<p>${item.robot_name} (Owner: ${item.robot_owner}, Starting Number: ${item.starting_number}. ${item.category_name})</p>`;
+                        const robotEntry = `
+                        <div class="bg-gray-800 text-white p-4 rounded-lg shadow-lg">
+                            <h4 class="text-xl font-black">${item.robot_name}</h4>
+                            <p class="mt-1"><span class="font-semibold">Owner: </span>${item.robot_owner}</p>
+                            <p><span class="font-semibold">Starting Number: </span>${item.starting_number}</p>
+                            <p><span class="font-semibold">Category: </span>${item.category_name}</p>
+                        </div>
+                        `;
                         startingList.innerHTML += robotEntry;
                     });
                 } else {
