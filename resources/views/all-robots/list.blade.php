301ecd4999506f1ddf2fcaf90be6d4ebd6bd398d
Tomáš Barták <tomasbartak12@email.cz>
2024-11-08 19:34:47 +0100

    feat: added All Robots layout with header

--- resources/views/all-robots/list.blade.php
+++ resources/views/all-robots/list.blade.php
@@ -5,7 +5,25 @@
         </h2>
     </x-slot>
 
-    @foreach($robots as $robot)
-        @include('all-robots.partials.robot-row', ['robot' => $robot])
-    @endforeach
+    <div class="max-w-7xl min-h-screen mx-auto dark:bg-gray-800 text-gray-900 dark:text-white shadow-md border border-gray-100 dark:border-gray-700">
+        <!-- Header Row -->
+        <div class="py-2 flex flex-row items-center sm:px-6 lg:px-8 px-10 gap-4 font-semibold text-gray-800 dark:text-gray-200 bg-gray-100 dark:bg-gray-700">
+            <!-- <div class="flex-1 flex flex-row"> if the robot name and author name should be closer -->
+                <div class="flex-1 text-lg">
+                    Robot Name
+                </div>
+                <div class="flex-1 text-lg">
+                    Author Name
+                </div>
+            <!-- </div> -->
+            <div class="flex-1 text-right text-lg">
+                Actions
+            </div>
+        </div>    
+        <div class="sm:px-6 lg:px-8 px-10">
+        @foreach($robots as $robot)
+            @include('all-robots.partials.robot-row', ['robot' => $robot])
+        @endforeach
+        </div>
+    </div>
 </x-app-layout>
--- resources/views/all-robots/partials/robot-row.blade.php
+++ resources/views/all-robots/partials/robot-row.blade.php
@@ -1,3 +1,20 @@
-<div class="pl-5 text-gray-900 dark:text-white">
-    {{ $robot->name }}
+<div class="flex flex-row items-center mx-auto gap-4 py-2 px-5 bg-white dark:bg-gray-800 text-gray-900 dark:text-white border-b" style="border-bottom: 1px solid transparent; border-image: linear-gradient(to right, rgba(55, 65, 81, 0) 0%, rgba(55, 65, 81, 1) 50%, rgba(55, 65, 81, 0) 100%) 1;">
+    <!-- <div class="flex-1 flex flex-row"> if the robot name and author name should be closer -->
+        <div class="flex-1 font-semibold truncate">
+            {{ $robot->name }}
+        </div>
+
+        <!-- Author Name -->
+        <div class="flex-1 font-semibold truncate">
+            {{ $robot->author_first_name }} {{ $robot->author_last_name }}
+        </div>
+    <!-- </div> -->
+
+    <!-- Action Button -->
+    <div class="flex gap-2 flex-1 justify-end">
+        <!-- Hide Button -->
+        <x-primary-button type="button" onclick="handleHideRobot({{ $robot->id }})">
+            Hide
+        </x-primary-button>
+    </div>
 </div>
