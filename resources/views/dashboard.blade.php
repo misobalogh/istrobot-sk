d0bf40807202297cf87230a7bf7dea583aebf111
Tomáš Barták <95168802+tbartak@users.noreply.github.com>
2024-11-09 17:17:21 +0100

    refactor: improve layout and spacing in dashboard view for better readability

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,37 +6,49 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                <div class="max-w-xl text-gray-900 dark:text-gray-100">
                     @if (Gate::allows('admin'))
                     {{ __("You're logged in as admin!") }}
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
-                        @include("dashboard.admin.starting-list", ['categories' => $categories])
-                    </div>
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
-                        @include("dashboard.admin.add-category")
-                    </div>
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
-                        @include("dashboard.admin.year-categories", ['categories' => $categories])
-                    </div>
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
-                        @include("dashboard.admin.emails")
-                    </div>
+                </div>
+            </div>
+            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                <div class="max-w-xl text-gray-900 dark:text-gray-100">
+                    @include("dashboard.admin.starting-list", ['categories' => $categories])
+                </div>
+            </div>
+            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                <div class="max-w-xl text-gray-900 dark:text-gray-100">
+                    @include("dashboard.admin.add-category")
+                </div>
+            </div>
+            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                <div class="max-w-xl text-gray-900 dark:text-gray-100">
+                    @include("dashboard.admin.year-categories", ['categories' => $categories])
+                </div>
+            </div>
+            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                <div class="max-w-xl text-gray-900 dark:text-gray-100">
+                    @include("dashboard.admin.emails")
+                </div>
+            </div>
                     @else
                     <!-- TODO: Remove later -->
                     {{ __("You're logged in!") }} 
                     @endif
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
+            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                <div class="max-w-xl text-gray-900 dark:text-gray-100">
                         @include("dashboard.profile-info")
                     </div>
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
-                        @include("dashboard.robot-register", [
-                            'categoriesForSetYear' => $categoriesForSetYear,
-                            'robots' => $robots,
-                            'robotsParticipation' => $robotsParticipation
-                            ]) 
-                    </div>
+            </div>
+            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+                <div class="max-w-xl text-gray-900 dark:text-gray-100">
+                    @include("dashboard.robot-register", [
+                        'categoriesForSetYear' => $categoriesForSetYear,
+                        'robots' => $robots,
+                        'robotsParticipation' => $robotsParticipation
+                        ]) 
                 </div>
             </div>
         </div>
