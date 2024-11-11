65beef8ce095a70d21715c6200eeda0e2fc3cbe1
Tomáš Barták <95168802+tbartak@users.noreply.github.com>
2024-11-11 20:00:17 +0100

    feat: add localization for login messages in English and Slovak

---
+++ resources/lang/en/login_messages.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    "email" => "Email",
+    "password" => "Password",
+    "remember_me" => "Remember me",
+    "forgot_password" => "Forgot your password?",
+    "login" => "Log In",
+];
---
+++ resources/lang/sk/login_messages.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    "email" => "Email",
+    "password" => "Heslo",
+    "remember_me" => "Zapamätať si ma",
+    "forgot_password" => "Zabudli ste heslo?",
+    "login" => "Prihlásiť sa",
+];
