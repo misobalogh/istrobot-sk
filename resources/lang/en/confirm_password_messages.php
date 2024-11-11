e56b49cce5543c2c8f954cf6d0057a71f840d993
Tomáš Barták <95168802+tbartak@users.noreply.github.com>
2024-11-11 19:49:36 +0100

    feat: add localization for confirm password messages in English and Slovak

---
+++ resources/lang/en/confirm_password_messages.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    "message" => "This is a secure area of the application. Please confirm your password before continuing.",
+    "password" => "Password",
+    "button" => "Confirm Password",
+];
---
+++ resources/lang/sk/confirm_password_messages.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    "message" => "Toto je bezpečná oblasť aplikácie. Pred pokračovaním potvrďte svoje heslo.",
+    "password" => "Heslo",
+    "button" => "Potvrdiť heslo",
+];
