48c3a0cd155e20e9a397a48081cfbb9215ae6c6f
Tomáš Barták <95168802+tbartak@users.noreply.github.com>
2024-11-11 20:12:35 +0100

    feat: add localization for email verification messages in English and Slovak

---
+++ resources/lang/en/verify_email_messages.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    "message" => "Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn't receive the email, we will gladly send you another.",
+    "verification_link" => "A new verification link has been sent to the email address you provided during registration.",
+    "resend_verification" => "Resend Verification Email",
+    "logout" => "Log Out",
+];
---
+++ resources/lang/sk/verify_email_messages.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    "message" => "Ďakujeme za registráciu! Predtým, ako začnete, môžete overiť svoju e-mailovú adresu kliknutím na odkaz, ktorý sme vám práve poslali e-mailom? Ak ste e-mail neobdržali, radi vám pošleme ďalší.",
+    "verification_link" => "Na e-mailovú adresu, ktorú ste poskytli počas registrácie, bol odoslaný nový overovací odkaz.",
+    "resend_verification" => "Znova odoslať overovací e-mail",
+    "logout" => "Odhlásiť sa",
+];
