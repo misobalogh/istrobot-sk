36cc8d80267e222527139870431090bb6b1371d8
xbedri04 <xbedri04@vutbr.cz>
2024-11-12 16:21:53 +0100

    feat: advanced contest layout, added iconds

--- resources/views/contest.blade.php
+++ resources/views/contest.blade.php
@@ -21,10 +21,9 @@
             <ul>
                 <li class="py-6 px-4 text-center hover:bg-gray-700 rounded">Novinky</li>
                 <li class="py-6 px-4 text-center hover:bg-gray-700 rounded">Pravidla</li>
-                <li class="py-6 px-4 text-center hover:bg-gray-700 rounded">Přihláška</li>
                 <li class="py-6 px-4 text-center hover:bg-gray-700 rounded">Roboty</li>
                 <li class="py-6 px-4 text-center hover:bg-gray-700 rounded">Archiv</li>
-                <li class="py-6 px-4 text-center hover:bg-gray-700 rounded">Roboty</li>                
+                <li class="py-6 px-4 text-center hover:bg-gray-700 rounded">Login</li>                
                 <li class="py-6 px-4 text-center hover:bg-gray-700 rounded flex justify-center items-center"><img src="{{ asset('img/icon/fb.png') }}" width="30px"></li>
                 <li class="py-6 px-4 text-center hover:bg-gray-700 rounded flex justify-center items-center"><img src="{{ asset('img/icon/yt.png') }}" width="30px"></li>
             </ul>
@@ -47,7 +46,7 @@
             <div class="w-full flex space-x-4" style="margin-top: 20px;">
                 <!-- First Child div -->
                 <div style="float: left; width: 25%;">
-                    <img src="{{ asset('img/tp/rand/foto05.jpg') }}" alt="robot" />
+                    <img src="{{ asset('img/tp/rand/foto05.jpg') }}" alt="robot" style="height: 100%; object-fit: cover;" />
                 </div>
 
                 <!-- Right Child div that takes up the remaining space -->
@@ -62,38 +61,95 @@
                 </div>
             </div>
 
+            <!-- Kategorie a pravidla -->
             <div class="w-full space-x-4" style="padding: 40px; background-color: rgb(6, 55, 74); color: lightgrey; font-size: 20px">
                 <p style="font-size: 30px"><b>Kategórie a pravidlá</b></p>
                 <p>Roboty súťažia v nasledujúcich kategóriách:</p>
 
-                <div class="flex w-full space-x-4" style="background-color: rgb(77, 108, 118);">
+                <!-- radek tabulky -->
+                <div class="flex w-full space-x-4" style="background-color: rgb(77, 108, 118); border-radius: 10px 10px 10px 10px;">
+                    <!-- sloupec v radku -->
                     <div style="padding: 30px; width: 50%; float: left; border-style: solid; border-width: 1px; border-color: lightgrey; border-radius: 10px 0px 0px 0px;">
-                        <div style="float left; width: 20%;" > 
-                            <img src="{{ asset('img/tp/rand/foto09.jpg') }}" alt="icon" width="80px" class="w-18 h-18 rounded-full" />
+                        <div style="float: left; width: 20%; height: 200px" > 
+                            <img src="{{ asset('img/icon/stoparICO.jpg') }}" alt="icon" width="80px" class="w-18 h-18 rounded-full" />
                         </div>
-                        <div style="float right; width: 80%;" > 
-                            
+                        <div style="float: right; width: 80%;" > 
+                            <h1 style="font-size: 25px;"><b>Stopár</b></h1>
+                            <p>Robot - stopár má čo najrýchlejšie prejsť zadanú dráhu a zdolať všetky jej nástrahy. Na dráhe sú umiestnené rozličné prekážky, napríklad prerušenie čiary alebo tehlička, ktorú treba obísť.</p>
+                            <button class="px-4 mt-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"> PODROBNOSTI </button>
                         </div>
                     </div>
+                    <!-- sloupec v radku -->
                     <div style="padding: 30px; width: 50%; float: right; border-style: solid; border-width: 1px; border-color: lightgrey; border-radius: 0px 10px 0px 0px;">
-
-                    </div>
-                
+                        <div style="float: left; width: 20%; height: 200px" > 
+                                <img src="{{ asset('img/icon/stoparICO.jpg') }}" alt="icon" width="80px" class="w-18 h-18 rounded-full" />
+                        </div>
+                        <div style="float: right; width: 80%;" > 
+                            <h1 style="font-size: 25px;"><b>V sklade kečupu</b></h1>
+                            <p>V tejto kategórii je úlohou zostrojiť robota, ktorý dokáže správne usporiadať konzervy s paradajkovým pretlakom v sklade. Súťažia vždy dva roboty, vyhráva ten, ktorý nazbiera viac plechoviek.</p>
+                            <button class="px-4 mt-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"> PODROBNOSTI </button>
+                        </div>
+                    </div>                
                 </div>
-                <div class="flex w-full space-x-4" style="background-color: rgb(77, 108, 118)">
+
+                <!-- radek tabulky -->
+                <div class="flex w-full space-x-4" style="background-color: rgb(77, 108, 118);">
+                    <!-- sloupec v radku -->
                     <div style="padding: 30px; width: 50%; float: left; border-style: solid; border-width: 1px; border-color: lightgrey;">
-
+                        <div style="float: left; width: 20%; height: 200px" > 
+                            <img src="{{ asset('img/icon/sumoICO.jpg') }}" alt="icon" width="80px" class="w-18 h-18 rounded-full" />
+                        </div>
+                        <div style="float: right; width: 80%;" > 
+                            <h1 style="font-size: 25px;"><b>LegoSumo</b></h1>
+                            <p>Dva roboty sa vytláčajú z kruhového ringu a ten silnejší zvíťazí. V tejto kategórií súťažia len roboty z Lega.</p>
+                            <button class="px-4 mt-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"> PODROBNOSTI </button>
+                        </div>
                     </div>
+                    <!-- sloupec v radku -->
                     <div style="padding: 30px; width: 50%; float: right; border-style: solid; border-width: 1px; border-color: lightgrey;">
-
-                    </div>
-                
+                        <div style="float: left; width: 20%; height: 200px" > 
+                                <img src="{{ asset('img/icon/mysICO.jpg') }}" alt="icon" width="80px" class="w-18 h-18 rounded-full" />
+                        </div>
+                        <div style="float: right; width: 80%;" > 
+                            <h1 style="font-size: 25px;"><b>Myš v bludisku</b></h1>
+                            <p>Autonómny robot - myš má čo najrýchlejšie nájsť cestu bludiskom. Pri hľadaní cesty bludiskom sa dá použiť pravidlo pravej, resp. ľavej ruky, ale takáto cesta nebude najkratšia.</p>
+                            <button class="px-4 mt-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"> PODROBNOSTI </button>
+                        </div>
+                    </div>                
                 </div>
+
+                <!-- radek tabulky -->
+                <div class="flex w-full space-x-4" style="background-color: rgb(77, 108, 118);padding: 30px; float: left; border-style: solid; border-width: 1px; border-color: lightgrey;">
+                    <div style="float: left; width: 10%; height: 130px" > 
+                        <img src="{{ asset('img/icon/walkICO.jpg') }}" alt="icon" width="80px" class="w-18 h-18 rounded-full" />
+                    </div>
+                    <div style="float: right; width: 90%;" > 
+                        <h1 style="font-size: 25px;"><b>Voľná jazda</b></h1>
+                        <p>Táto kategória je určená na predvádzanie vašich robotov. Každý súťažiaci môže predviesť všetko, čo jeho robot dokáže. O víťazovi rozhodne porota na základe prezentácie robota a rozhovoru s autorom.</p>
+                        <button class="px-4 mt-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"> PODROBNOSTI </button>
+                    </div>
+                </div>
+
+                <!-- radek tabulky -->
+                <div class="flex w-full space-x-4" style="background-color: rgb(77, 108, 118);padding: 30px; float: left; border-style: solid; border-width: 1px; border-color: lightgrey; border-radius: 0px 0px 10px 10px ">
+                    <div style="float: left; width: 10%; height: 130px" > 
+                        <img src="{{ asset('img/icon/volnajazdaICO.jpg') }}" alt="icon" width="80px" class="w-18 h-18 rounded-full" />
+                    </div>
+                    <div style="float: right; width: 90%;" > 
+                        <h1 style="font-size: 25px;"><b>Společné pravidlá</b></h1>
+                        <p>Naďalej platia pre všetky kategórie spoločné pravidlá týkajúce sa hlavne bezpečnosti a použitých materiálov.</p>
+                        <button class="px-4 mt-4 py-2 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75"> PODROBNOSTI </button>
+                    </div>
+                </div>
+
             </div>
 
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu porttitor enim. Sed sed turpis dui. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam erat volutpat. Sed blandit rutrum rhoncus. Vestibulum volutpat ante ut nisi facilisis, quis accumsan purus vulputate. Suspendisse magna nunc, varius sed feugiat quis, venenatis non leo.
-
-        Nam dolor turpis, cursus ac dictum id, elementum nec massa. Integer tempor nisi vel dui vulputate vehicula. Etiam vehicula volutpat tellus, in placerat justo cursus et. Nam vestibulum tristique sollicitudin. Nulla elementum consequat porta. Pellentesque nulla turpis, ultricies ac massa id, laoreet posuere massa. Proin non sem at velit tincidunt vulputate. Donec nec leo at nunc dignissim venenatis. Fusce fermentum velit non nunc feugiat, at posuere risus eleifend. Mauris eu nisl eget tellus vestibulum dictum.
+            <div class="w-full space-x-4" style="padding: 40px;  background-color: rgb(75, 75, 75); font-size: 20px; color: lightgrey">
+                <b style="font-size:30px;">Ochrana osobných údajov (GDPR)</b>
+                <p>Registráciou a vyplnením prihlášky na súťaž Istrobot účastníci súhlasia so zverejnením svojich osobných údajov v podobe prihlášky, štartovej listiny a výsledkov na webe súťaže (istrobot.sk a robotika.sk). Mená víťazov sa môžu objaviť aj v tlačovej správe a médiach, ako aj na sociálnych sieťach (facebook, Instagram, YouTube). Súťažiaci súhlasí so zverejnením svojho videa na stránkach súťaže a sociálnych sieťach (napr. istrobot.sk, robotika.sk, facebook, Instagram, YouTube) samozrejme s uvedením autora.</p>
+                <br />
+                <p><b>Fotografovanie a video:</b> Naše podujatia sú verejné. Vaše osobné údaje v rozsahu fotografií a videozáznamov, týkajúce sa osoby účastníka alebo jeho prejavov osobnej povahy v súvislosti s vašou účasťou na podujatí, spracúvame za účelom ich získania (vyhotovenia) a použitia, a to formou ich zverejnenia na webovej stránke robotika.sk a istrobot.sk, na sociálnych sieťach, v tlačových správach a u spoluorganizátorov a na účel propagácie organizátorov a robotiky. Vezmite, prosím, na vedomie, že počas podujatia budú takéto záznamy vyhotovované a že účasťou vyjadrujete súhlas s týmto spracúvaním osobných údajov. V prípade nesúhlasu máte možnosť namietať proti vyhotovovaniu obrazového/obrazovo-zvukového záznamu svojej osoby u fotografa/kameramana.</p>
+            </div>
         </div>
     </body>
 
