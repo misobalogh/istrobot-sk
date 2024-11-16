cbdb37554328b6be9c4894fccf265d2e3e2668ea
Tomáš Barták <95168802+tbartak@users.noreply.github.com>
2024-11-16 13:11:16 +0100

    feat: enhance styling of registered robots table for better layout

--- resources/views/contest/registered_robots.blade.php
+++ resources/views/contest/registered_robots.blade.php
@@ -6,18 +6,18 @@
                 @forelse ($registered_robots as $category => $robots)
                 <h2 class="font-semibold mt-4">{{ $category }}</h2>
                 <div class="overflow-x-auto">
-                    <table class="min-w-full bg-white dark:bg-gray-800 rounded shadow">
+                    <table class="min-w-full bg-white dark:bg-gray-800 rounded-lg overflow-hidden shadow">
                         <thead>
-                            <tr class="border-b dark:border-gray-700">
-                                <th class="px-4 py-2 text-left">{{ __('registered_robots_messages.robot_name') }}</th>
-                                <th class="px-4 py-2 text-left">{{ __('registered_robots_messages.author') }}</th>
+                            <tr class="border-b dark:border-gray-700 flex">
+                                <th class="px-4 py-2 text-left flex-1">{{ __('registered_robots_messages.robot_name') }}</th>
+                                <th class="px-4 py-2 text-left flex-1">{{ __('registered_robots_messages.author') }}</th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach ($robots as $robot)
-                            <tr class="border-b dark:border-gray-700">
-                                <td class="px-4 py-2">{{ $robot['name'] }}</td>
-                                <td class="px-4 py-2">{{ $robot['author'] }}</td>
+                            <tr class="border-b dark:border-gray-700 flex last:border-b-0">
+                                <td class="px-4 py-2 flex-1">{{ $robot['name'] }}</td>
+                                <td class="px-4 py-2 flex-1">{{ $robot['author'] }}</td>
                             </tr>
                             @endforeach
                         </tbody>
