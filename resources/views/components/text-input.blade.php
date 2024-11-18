ea12ebde5902bc867a44d513d60840f37847037e
Tomáš Barták <95168802+tbartak@users.noreply.github.com>
2024-11-18 13:32:02 +0100

    fix: add name attribute to text input component

--- resources/views/components/text-input.blade.php
+++ resources/views/components/text-input.blade.php
@@ -6,7 +6,7 @@
 ])
 
 <div>
-    <input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) !!}
+    <input name="{{ $name }}" {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm']) !!}
         @if($maxLength)
                     maxlength="{{ $maxLength }}"
                     oninput="validateInputLength(this, '{{ $name }}_error')"
